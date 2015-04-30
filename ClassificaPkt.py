046c29dfe10e50558b7c25a43118aa80299276d7
Nicollas21 <cnicollas21@hotmail.com>
2015-04-30 16:02:53 -0700

    Classe dominio capturar, classificar e enviar pacotes

--- ClassificaPkt.py
+++ ClassificaPkt.py
@@ -8,9 +8,8 @@ class ClassificaPkt():
     protocols = {"bittorrent":"","dhcp":"","http":"","ssdp":"","ssh":"","ssl":""}
 
     maxPkts = 100
-    cnt = {"bittorrent":0,"dhcp":0,"http":0,"ssdp":0,"ssh":0,"ssl":0,"noClass":0}
+    cnt = {"bittorrent":0,"dhcp":0,"http":0,"ssdp":0,"ssh":0,"ssl":0,"unknown":0}
     #contadores
-    cNonIP = 0
     cNonIP = 0
     
     def get_arquivo(self,Dir):
@@ -43,29 +42,33 @@ class ClassificaPkt():
         for p in self.protocols.items():
             if p[1].search(app):
                 tamanho = len(eth)
-                tupla = (tamanho,ts,"eth","ip","tcp",p[0])
-                self.enviar_pkt(tupla)
+                tupla = tamanho,ts,"eth","ip","tcp",p[0]
+                self.modifier_tupla(tupla)
                 self.cnt[p[0]] += 1
                 found = True
         if (not found):
-            self.cnt["noClass"] += 1
+            self.cnt["unknown"] += 1
+            tupla = "unknown","tcp","sem info"
+            self.modifier_tupla(tupla)
 
     def get_protocol_trans_udp(self,app,eth,ts):
         found = False
         for p in self.protocols.items():
             if p[1].search(app):
                 tamanho = len(eth)
-                tupla = (tamanho,ts,"eth","ip","udp",p[0])
-                self.enviar_pkt(tupla)
+                tupla = tamanho,ts,"eth","ip","udp",p[0]
+                self.modifier_tupla(tupla)
                 self.cnt[p[0]] += 1
                 found = True
         if (not found):
-            self.cnt["noClass"] += 1
+            self.cnt["unknown"] += 1
+            tupla = "unknown","udp","sem info"
+            self.modifier_tupla(tupla)
         
     def classificar_protocol(self, protocols):
-        
+        #nPkts=0
         for ts, pkt in pcap.pcap('test-capture.pcap'):
-            
+            #nPkts += 1
             
             eth = dpkt.ethernet.Ethernet(pkt) #extraindo dados do pacote
             ip = eth.data
@@ -80,23 +83,38 @@ class ClassificaPkt():
                             
             else:
                     self.cNonIP += 1
+                    tupla = "unknown","sem info"
+                    self.modifier_tupla(tupla)
 
+            #if (nPkts == self.maxPkts):
+                    #break
             
         for p in self.cnt.items():
             print(p[0]+" Pkts:"+str(p[1]))
         print("Non IP Pkts:"+str(self.cNonIP))
 
-    def enviar_pkt(self,tupla):
-        #print tupla
-
-        tupla = str(tupla)
-        tupla = tupla.replace(" ","")
-        tupla = tupla.replace("(","")
-        tupla = tupla.replace(")","")
-        tupla = tupla.replace("''","")
-        teste = tupla.split(',')
-        nome_protocolo = str(teste[5].replace("'",""))
-        #print teste[5].replace("'","")
+    def modifier_tupla(self,tupla):
+        #print type(tupla[0])
+        if type(tupla[0]) is int:
+            tupla = str(tupla)
+            tupla = tupla.replace(" ","")
+            tupla = tupla.replace("(","")
+            tupla = tupla.replace(")","")
+            teste = tupla.split(',')
+            nome_protocolo = str(teste[5].replace("'",""))
+            print nome_protocolo
+            #self.envia_tupla(nome_protocolo,teste)
+        else:
+            tupla = str(tupla)
+            tupla = tupla.replace(" ","")
+            tupla = tupla.replace("(","")
+            tupla = tupla.replace(")","")
+            teste = tupla.split(',')
+            nome_protocolo = str(teste[0].replace("'",""))
+            print nome_protocolo
+            #self.envia_tupla(nome_protocolo,teste)
+            
+    def envia_tupla(nome_protocolo, msg):
         
         credentials = pika.PlainCredentials('server', 'server123')
         connection = pika.BlockingConnection(pika.ConnectionParameters(
@@ -106,13 +124,13 @@ class ClassificaPkt():
         channel.exchange_declare(exchange='topic_logs',
                          type='topic')
 
-        routing_key = nome_protocolo if len(teste) > 1 else 'anonymous.info'
+        routing_key = nome_protocolo if len(msg) > 1 else 'anonymous.info'
         print routing_key
-        message = str(teste) or 'Hello World!'
+        message = str(msg) or 'Hello World!'
         channel.basic_publish(exchange='topic_logs',
                       routing_key=routing_key,
                       body=message)
         print " [x] Sent %r:%r" % (routing_key, message)
         connection.close()
-            
+        
         
