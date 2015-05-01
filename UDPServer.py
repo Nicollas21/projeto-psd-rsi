d1b5a661f6d5872de84baf15ef4a81534099bfdf
Nicollas21 <cnicollas21@hotmail.com>
2015-05-01 11:25:44 -0700

    comunicacao server udp

---
+++ UDPServer.py
@@ -0,0 +1,28 @@
+# -*- coding: cp1252 -*-
+from socket import *
+
+serverPort = 12000
+#Cria o Socket UDP (SOCK_DGRAM) para rede IPv4 (AF_INET)
+serverSocket = socket(AF_INET, SOCK_DGRAM)
+#Associa o Socket criado com a porta desejada
+serverSocket.bind(('', serverPort))
+
+print("Servidor pronto para receber mensagens. Digite Ctrl+C para terminar.")
+
+while 1:
+    try:
+	#Aguarda receber dados do socket
+        message, clientAddress = serverSocket.recvfrom(2048)
+        if message.upper() == "EXIT":
+            modifiedMessage = message.upper()
+            serverSocket.sendto(modifiedMessage, clientAddress)
+            break
+        else:
+            print(clientAddress)
+            modifiedMessage = message.upper()
+            serverSocket.sendto(modifiedMessage, clientAddress)
+    except (KeyboardInterrupt, SystemExit):
+	break
+
+serverSocket.close()
+
