40362581c616b137b731ba843739b07bac7a8cc2
Nicollas21 <cnicollas21@hotmail.com>
2015-05-28 13:20:07 -0700

    arquivo de executa a classe ReceiveCommand

--- main.py
+++ main.py
@@ -6,20 +6,6 @@ from threading import Thread
 
 
 k = ReceiveCommand()
-
-y = ClassificaPkt()
-bit = y.get_arquivo('l7-pat/bittorrent.pat')
-dhcp = y.get_arquivo('l7-pat/dhcp.pat')
-http = y.get_arquivo('l7-pat/http.pat')
-ssdp = y.get_arquivo('l7-pat/ssdp.pat')
-ssh = y.get_arquivo('l7-pat/ssh.pat')
-ssl = y.get_arquivo('l7-pat/ssl.pat')
-protocol = y.assinar_protocols(bit,dhcp,http,ssdp,ssh,ssl)
-
-
 t1 = Thread(target=k.startListen,args=())
 t1.start()
-"""
-t2 = Thread(target=y.classificar_protocol,args=(protocol,"",))
-t2.start()
-"""
+
